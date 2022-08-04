a5acf00ce2ec71dfc45ba46f979fe8ac5a7d620e
Tris <triskominfoss@gmail.com>
2022-08-04 14:11:15 +0700

    app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css';
 
 // import Home from './pages/Home';
